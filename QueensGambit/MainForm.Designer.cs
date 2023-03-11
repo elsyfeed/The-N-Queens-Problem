9adfae0244a7485a22319444a35b285934e9fed2
Elsy <Elsy@DESKTOP-3QM9NCH>
2023-03-12 00:12:37 +0200

    Add project files.

---
+++ QueensGambit/MainForm.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace QueensGambit
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(46)))), ((int)(((byte)(44)))));
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(12, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(272, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Choose amount of Queens";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(46)))), ((int)(((byte)(44)))));
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(12, 342);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(339, 30);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "The number of solutions reaches: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(46)))), ((int)(((byte)(44)))));
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(12, 378);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(262, 30);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "List of available solutions: ";
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.Honeydew;
+            this.listBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 21;
+            this.listBox1.Location = new System.Drawing.Point(12, 416);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(310, 361);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.Click += new System.EventHandler(this.listBox1_Click);
+            this.listBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.listBox1_KeyUp);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(46)))), ((int)(((byte)(44)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(12, 236);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(287, 90);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Solve the problem ▦";
+            this.button1.UseMnemonic = false;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(46)))), ((int)(((byte)(44)))));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.Transparent;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Location = new System.Drawing.Point(447, 113);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridView1.Size = new System.Drawing.Size(820, 786);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(46)))), ((int)(((byte)(44)))));
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(745, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(183, 41);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Chess board";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(46)))), ((int)(((byte)(44)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(12, 809);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(287, 90);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Export as PNG ↓ ";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.Honeydew;
+            this.comboBox1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 168);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 38);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1347, 975);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "N Queens problem";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private ListBox listBox1;
+        private Button button1;
+        private DataGridView dataGridView1;
+        private Label label4;
+        private Button button2;
+        private ComboBox comboBox1;
+    }
+}
---
+++ QueensGambit/MainForm.cs
@@ -0,0 +1,337 @@
+﻿using System.Diagnostics;
+using System.Drawing.Imaging;
+
+namespace QueensGambit
+{
+    public partial class MainForm : Form
+    {
+        private int N;
+
+        bool InDamage(int x1, int y1, int x2, int y2)
+        {
+            if ((x1 == x2) || (y1 == y2))
+                return true;
+
+
+            int tx, ty;
+
+            tx = x1 - 1;
+            ty = y1 - 1;
+            while ((tx >= 1) && (ty >= 1))
+            {
+                if ((tx == x2) && (ty == y2))
+                    return true;
+                tx--;
+                ty--;
+            }
+
+            tx = x1 + 1;
+            ty = y1 + 1;
+            while ((tx <= N) && (ty <= N))
+            {
+                if ((tx == x2) && (ty == y2))
+                    return true;
+                tx++;
+                ty++;
+            }
+
+            tx = x1 + 1;
+            ty = y1 - 1;
+            while ((tx <= N) && (ty >= 1))
+            {
+                if ((tx == x2) && (ty == y2))
+                    return true;
+                tx++;
+                ty--;
+            }
+
+            tx = x1 - 1; ty = y1 + 1;
+            while ((tx >= 1) && (ty <= N))
+            {
+                if ((tx == x2) && (ty == y2))
+                    return true;
+                tx--;
+                ty++;
+            }
+
+            return false;
+        }
+
+        bool ChekDamage(int[] M, int p)
+        {
+            int px, py, x, y;
+            px = M[p];
+            py = p;
+
+            for (int i = 1; i <= p - 1; i++)
+            {
+                x = M[i];
+                y = i;
+                if (InDamage(x, y, px, py))
+                    return true;
+            }
+
+            return false;
+        }
+
+        void InitDataGridView(int N)
+        {
+            dataGridView1.Columns.Clear();
+
+            for (int i = 0; i < N; i++)
+            {
+                dataGridView1.Columns.Add($"{i + 1}" + i.ToString(), i.ToString());
+
+
+                dataGridView1.Columns[i].Width = 20;
+            }
+
+            dataGridView1.Rows.Add(N);
+
+            for (int i = 0; i < N; i++)
+            {
+                dataGridView1.Rows[i].HeaderCell.Value = (i + 1).ToString();
+            }
+
+            dataGridView1.AllowUserToAddRows = false;
+
+            dataGridView1.RowsDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+        }
+
+        private void dataGridView1_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            if (e.RowIndex % 2 == 0)
+            {
+                if (e.ColumnIndex % 2 == 0)
+                {
+                    e.CellStyle.BackColor = Color.White;
+                    e.CellStyle.ForeColor = Color.Black;
+                }
+                else
+                {
+                    e.CellStyle.BackColor = Color.Black;
+                    e.CellStyle.ForeColor = Color.White;
+                }
+            }
+            else
+            {
+                if (e.ColumnIndex % 2 == 0)
+                {
+                    e.CellStyle.BackColor = Color.Black;
+                    e.CellStyle.ForeColor = Color.White;
+                }
+                else
+                {
+                    e.CellStyle.BackColor = Color.White;
+                    e.CellStyle.ForeColor = Color.Black;
+                }
+            }
+        }
+
+        void ShowDataGridView(string s, int N)
+        {
+            string xs, ys;
+            int x, y;
+            int j;
+
+            for (int i = 0; i < N; i++)
+            {
+                for (j = 0; j < N; j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = "";
+                }
+            }
+
+            j = 0;
+            for (int i = 0; i < N; i++)
+            {
+                xs = "";
+                while (s[j] != ',')
+                {
+                    xs = xs + s[j].ToString();
+                    j++;
+                }
+                j++;
+
+                ys = "";
+                while (s[j] != '-')
+                {
+                    ys = ys + s[j].ToString();
+                    j++;
+                }
+                j++;
+
+                x = Convert.ToInt32(xs);
+                y = Convert.ToInt32(ys);
+
+                dataGridView1.Rows[y - 1].Cells[x - 1].Value = "♕";
+            }
+        }
+
+        void AddToListBox(int[] M, int N)
+        {
+            string s = "";
+            for (int i = 1; i <= N; i++)
+                s = s + M[i].ToString() + "," + i.ToString() + "-";
+
+            listBox1.Items.Add(s);
+        }
+
+        private void listBox1_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count <= 0)
+                return;
+
+            int num = listBox1.SelectedIndex;
+            string s = listBox1.Items[num].ToString();
+            ShowDataGridView(s, N);
+        }
+
+        private void listBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (listBox1.Items.Count <= 0) return;
+
+            int num;
+            string s;
+            num = listBox1.SelectedIndex;
+            s = listBox1.Items[num].ToString();
+            ShowDataGridView(s, N);
+        }
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            const int MaxN = 20;
+            int[] M = new int[MaxN];
+            int p;
+            int k;
+
+            int number;
+            if (int.TryParse(comboBox1.Text, out number))
+            {
+                N = Convert.ToInt32(comboBox1.Text);
+                InitDataGridView(N);
+                dataGridView1.CellFormatting += dataGridView1_CellFormatting;
+
+                if (N > 10)
+                {
+
+
+                    foreach (DataGridViewColumn column in dataGridView1.Columns)
+                    {
+                        column.Width = 70;
+
+
+                    }
+                    foreach (DataGridViewRow row in dataGridView1.Rows)
+                    {
+                        row.Height = 70;
+                    }
+
+                    dataGridView1.Width = 70 * N;
+                    dataGridView1.Height = 70 * N;
+
+
+                    dataGridView1.DefaultCellStyle.Font = new Font("Arial", 33, FontStyle.Bold);
+                }
+                else
+                {
+                    foreach (DataGridViewColumn column in dataGridView1.Columns)
+                    {
+                        column.Width = 85;
+
+
+                    }
+                    foreach (DataGridViewRow row in dataGridView1.Rows)
+                    {
+                        row.Height = 85;
+                    }
+                    dataGridView1.Width = 85 * N;
+                    dataGridView1.Height = 85 * N;
+                    dataGridView1.DefaultCellStyle.Font = new Font("Arial", 45, FontStyle.Bold);
+                }
+                dataGridView1.CurrentCell = null;
+
+                dataGridView1.RowHeadersVisible = false;
+                dataGridView1.ColumnHeadersVisible = false;
+
+                listBox1.Items.Clear();
+
+
+                p = 1;
+                M[p] = 0;
+                M[0] = 0;
+                k = 0;
+
+
+                while (p > 0)
+                {
+                    M[p] = M[p] + 1;
+                    if (p == N)
+                    {
+                        if (M[p] > N)
+                        {
+                            while (M[p] > N) p--;
+                        }
+                        else
+                        {
+                            if (!ChekDamage(M, p))
+                            {
+
+                                AddToListBox(M, N);
+                                k++;
+                                p--;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (M[p] > N)
+                        {
+                            while (M[p] > N) p--;
+                        }
+                        else
+                        {
+                            if (!ChekDamage(M, p))
+                            {
+                                p++;
+                                M[p] = 0;
+                            }
+                        }
+                    }
+                }
+
+                if (k > 0)
+                {
+                    listBox1.SelectedIndex = 0;
+                    listBox1_Click(sender, e);
+                    label2.Text = "The number of solutions reaches: " + k.ToString();
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap bitmap = new Bitmap(dataGridView1.Width, dataGridView1.Height);
+            dataGridView1.DrawToBitmap(bitmap, new Rectangle(0, 0, this.dataGridView1.Width, this.dataGridView1.Height));
+
+            string imagePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), "N Queens Problem solution.png");
+            bitmap.Save(imagePath, ImageFormat.Png);
+            Process.Start("explorer.exe", "/select, \"" + imagePath + "\"");
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
